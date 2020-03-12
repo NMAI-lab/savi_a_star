25c8c2681bf542f2bfe69aa0ade76e508d736ddf
Patrick Gavigan <pgavigan@gmail.com>
2020-03-11 23:29:31 -0400

    First fully working version
    Need to add map loader

--- scripts/RouteSearcher.py
+++ scripts/RouteSearcher.py
@@ -1,31 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 11 20:40:10 2020
+#!/usr/bin/env python
 
-@author: Patrick
-"""
+# Definitions for the search functionality
+
+# @author: Patrick Gavigan
 
 from astar import AStar
 import math
 
 class RouteSearcher(AStar):
 
-    """sample use of the astar algorithm. In this exemple we work on a maze made of ascii characters,
-    and a 'node' is just a (x,y) tuple that represents a reachable position"""
-
+    # Define the map
     def __init__(self):
+        
+        # Distances between neighbouring nodes as a graph
         self.nodeGraph = {'A': [('B',5), ('D',5)],
                           'B': [('A',5), ('C',5)],
                           'C': [('B',5), ('D',5), ('E',5)],
                           'D': [('A',5), ('C',5)],
                           'E': [('C',5)]}
         
+        # Absolute locations of each node
         self.nodeLocations = {'A': (0,0),
                               'B': (0,5),
                               'C': (5,5),
                               'D': (5,0),
-                              'E': (5,10)}
-        
+                              'E': (5,10)}        
 
      # Compute the distance between two (x,y) tuples
     def heuristic_cost_estimate(self, n1, n2):      
@@ -42,5 +41,3 @@ class RouteSearcher(AStar):
         neighbourNodes = self.nodeGraph[node]
         neighbourNames = [a_tuple[0] for a_tuple in neighbourNodes]
         return neighbourNames
-    
-    
